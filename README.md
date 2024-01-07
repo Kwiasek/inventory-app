205095524f73298d489109aeaa7e2aac63bddc21
Adam Kwiasowski <kwiasekkwiasek@gmail.com>
2024-01-07 15:36:31 +0100

    Added middleware to protect against well known vulnerabilities

---
+++ README.md
@@ -0,0 +1,5 @@
+# Inventory App
+
+Simple inventory management app that allows user to do CRUD operations on database within browser
+
+All db queries are processed by `express-validator` to escape unwanted
--- app.js
+++ app.js
@@ -4,11 +4,31 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 require("dotenv").config();
+const compression = require("compression");
+const helmet = require("helmet");
 
 const indexRouter = require("./routes/index");
 const inventoryRouter = require("./routes/inventory");
 
 const app = express();
+
+app.use(compression());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+);
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
 
 // db connection
 const mongoose = require("mongoose");
